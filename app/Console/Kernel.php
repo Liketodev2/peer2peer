699e235814520f14725dff0b6e55ae94f2431974
liketodev2 <liketodev2@gmail.com>
2021-07-12 15:53:43 +0400

    cron everyminute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('rss:read')->daily();
+         $schedule->command('rss:read')->everyMinute();
          $schedule->command('remove:old')->yearly();
     }
 
