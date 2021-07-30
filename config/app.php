084c34168b79463c04fb4c6bdb1400cfc6db3b98
liketodev2 <liketodev2@gmail.com>
2021-07-30 11:10:13 +0400

    my channels

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+        //Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -230,7 +230,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'FeedReader' => Vedmant\FeedReader\Facades\FeedReader::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        //'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
