fc500095acfca13987e8ef5b39d324862fd242d9
liketodev2 <liketodev2@gmail.com>
2021-08-13 13:09:04 +0400

    test

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -70,7 +70,6 @@
                                     @else
                                         <p class="text-center mt-3 text-dark"><i class="far fa-folder-open mr-2"></i> Notifications are empty</p>
                                     @endif
-
                                     <div>
                                         <a href="{{route('notifications')}}" role="button" class="btn-red px-5">View all in full window</a>
                                     </div>
