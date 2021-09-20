1ba04e6c9298a79f5db6c6ccb392d4586ab179e9
liketodev2 <liketodev2@gmail.com>
2021-09-20 10:47:22 +0400

    rss

--- app/Console/Commands/RssReader.php
+++ app/Console/Commands/RssReader.php
@@ -69,9 +69,11 @@ class RssReader extends Command
 
                 $to_search = $title;
                 $res_search = $check_unique->filter(function ($item) use ($to_search) {
-                    if($item->title && $to_search){
-                        return false !== stristr($item->title, $to_search);
+
+                    if(!$item->title || !$to_search){
+                        return false;
                     }
+                    return false !== stristr($item->title, $to_search);
                 });
 
                 if($res_search && $res_search->count() == 0){
