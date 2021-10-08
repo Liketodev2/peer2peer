96e88f1bb74e9274c6f85896e810144f7b65ec3a
liketodev2 <liketodev2@gmail.com>
2021-10-08 14:38:47 +0400

    trending changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Category;
 use App\Models\Feed;
 use App\Models\Notify;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
@@ -36,11 +37,44 @@ class AppServiceProvider extends ServiceProvider
         }
 
         if(Schema::hasTable('feeds')) {
-            $trending_feeds = [];
-            $category = Category::where('name', 'like', '%' . 'World' . '%')->first();
-            if($category){
-                $trending_feeds = Feed::where('category_id',$category->id)->withCount('likes_pivot')->orderBy('likes_pivot_count','desc')->published()->take(5)->get();
-            }
+
+                $trending_feeds = [];
+                $category = Category::where('name', 'like', '%' . 'Local' . '%')->first();
+                $trending_feeds = Feed::where('created_at', '>=', Carbon::now()->subDay()->toDateTimeString());
+
+                if($category){
+                    $trending_feeds = $trending_feeds->where('category_id', '!=', $category->id);
+                }
+
+                $trending_feeds = $trending_feeds
+                    ->withCount('comments')
+                    ->having('comments_count', '>', 0)
+                    ->orderBy('comments_count','desc')
+                    ->published()
+                    ->limit(6)
+                    ->get();
+
+                if($trending_feeds->count() < 5 && $trending_feeds->count() > 0){
+                    $average = 5 - $trending_feeds->count();
+                    $trending_feeds_part = Feed::where('created_at', '>=', Carbon::now()->subDay()->toDateTimeString())
+                        ->published()
+                        ->inRandomOrder()
+                        ->limit($average)
+                        ->get();
+
+                    if($trending_feeds_part){
+                        $trending_feeds = $trending_feeds->merge($trending_feeds_part);
+                    }
+                }else{
+                    $trending_feeds_part = Feed::where('created_at', '>=', Carbon::now()->subDay()->toDateTimeString())
+                        ->published()
+                        ->inRandomOrder()
+                        ->limit(5)
+                        ->get();
+                    $trending_feeds = $trending_feeds_part;
+                }
+
+
             View::share('trending_feeds', $trending_feeds);
         }
 
--- resources/views/areas/feed-right-side.blade.php
+++ resources/views/areas/feed-right-side.blade.php
@@ -2,26 +2,6 @@
     $feeds = \App\Http\Controllers\FunctionController::getLimetedFeeds();
 @endphp
 <aside class="asides right_aside">
-    <div class="border-bottom pb-4 px-2 py-3">
-        <div class="title">Tech</div>
-        <ul class="m-0 p-0">
-            @if($feeds['tech']->count() > 0)
-                @foreach($feeds['tech'] as $feed)
-                    <li><a href=" {{route('feed', $feed->id)}} ">{{$feed->title}}</a></li>
-                @endforeach
-            @endif
-        </ul>
-    </div>
-    <div class="border-bottom pb-4 px-2 py-3">
-        <div class="title">Health</div>
-        <ul class="m-0 p-0">
-            @if($feeds['health']->count() > 0)
-                @foreach($feeds['health'] as $feed)
-                    <li><a href=" {{route('feed', $feed->id)}} ">{{$feed->title}}</a></li>
-                @endforeach
-            @endif
-        </ul>
-    </div>
     <div class="aside-accordion alert p-0">
         <div class="btn btn-red w-100 d-flex justify-content-between align-items-center">
 
@@ -44,5 +24,25 @@
                 </ul>
             </div>
         </div>
+    </div>
+    <div class="border-bottom pb-4 px-2 py-3">
+        <div class="title">Tech</div>
+        <ul class="m-0 p-0">
+            @if($feeds['tech']->count() > 0)
+                @foreach($feeds['tech'] as $feed)
+                    <li><a href=" {{route('feed', $feed->id)}} ">{{$feed->title}}</a></li>
+                @endforeach
+            @endif
+        </ul>
+    </div>
+    <div class="border-bottom pb-4 px-2 py-3">
+        <div class="title">Health</div>
+        <ul class="m-0 p-0">
+            @if($feeds['health']->count() > 0)
+                @foreach($feeds['health'] as $feed)
+                    <li><a href=" {{route('feed', $feed->id)}} ">{{$feed->title}}</a></li>
+                @endforeach
+            @endif
+        </ul>
     </div>
 </aside>
