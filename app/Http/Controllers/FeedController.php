172ffa2fa3b3c0bb06234631d97b86fbdc7f6126
liketodev2 <liketodev2@gmail.com>
2021-10-20 11:14:09 +0400

    curl options change

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -221,6 +221,13 @@ class FeedController extends Controller
         function file_get_contents_curl($url)
         {
             $curl = curl_init();
+            $agents = array(
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+                'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.9) Gecko/20100508 SeaMonkey/2.0.4',
+                'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
+                'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; da-dk) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1'
+
+            );
 
             curl_setopt_array($curl, array(
                 CURLOPT_URL => $url,
@@ -233,6 +240,7 @@ class FeedController extends Controller
                 CURLOPT_SSL_VERIFYPEER => false,
                 CURLOPT_CUSTOMREQUEST => 'GET',
                 CURLOPT_CONNECTTIMEOUT => 0,
+                CURLOPT_USERAGENT => $agents[array_rand($agents)]
             ));
 
             $data = curl_exec($curl);
