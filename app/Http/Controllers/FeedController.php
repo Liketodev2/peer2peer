7de806dad26162a53fbe2f2bff4a1fd9f5d02df7
liketodev2 <liketodev2@gmail.com>
2021-10-20 11:46:21 +0400

    curl settings headers

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -229,6 +229,16 @@ class FeedController extends Controller
 
             );
 
+            $header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,";
+            $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+            $header[] = "Cache-Control: max-age=0";
+            $header[] = "Connection: keep-alive";
+            $header[] = "Keep-Alive: 300";
+            $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+            $header[] = "Accept-Language: en-us,en;q=0.5";
+            $header[] = "Pragma: ";
+
+
             curl_setopt_array($curl, array(
                 CURLOPT_URL => $url,
                 CURLOPT_RETURNTRANSFER => true,
@@ -237,6 +247,7 @@ class FeedController extends Controller
                 CURLOPT_TIMEOUT => 0,
                 CURLOPT_FOLLOWLOCATION => true,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_HTTPHEADER => $header,
                 CURLOPT_SSL_VERIFYPEER => false,
                 CURLOPT_CUSTOMREQUEST => 'GET',
                 CURLOPT_CONNECTTIMEOUT => 0,
