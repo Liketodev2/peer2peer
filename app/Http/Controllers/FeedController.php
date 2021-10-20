2750d5d3f2bb045cd038808f645091229a4bd6fa
liketodev2 <liketodev2@gmail.com>
2021-10-20 15:21:22 +0400

    changes

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -220,7 +220,9 @@ class FeedController extends Controller
 
         try {
             $httpClient = new \GuzzleHttp\Client();
-            $response = $httpClient->get($request->url);
+            $response = $httpClient->request('GET', $request->url, ['headers' => ['user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+                'accept'=>'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9']]);
+
             $htmlString = (string)$response->getBody();
 
             $doc = new \DOMDocument();
